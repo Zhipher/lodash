68b1c3ed3c27b490027c02b698b53dd3aea48423
tnte4930 <contact@webmayeur.com>
2022-06-08 18:07:32 +0200

    exo10 terminé

---
+++ exercices/exo11.ts
@@ -0,0 +1,13 @@
+// 
+// Tableau de tous les enfants
+// 
+
+import map from 'lodash/map'
+import flatten from 'lodash/flatten'
+import uniq from 'lodash/uniq'
+
+import { children } from './persons'
+
+const ages = map(children, ({ childName }) => childName.age)
+
+console.log(ages)
