866ff3bf0949a23232550940b38b811e5a1a7bc3
tnte4930 <contact@webmayeur.com>
2022-06-09 15:24:59 +0200

    upperFirst

---
+++ types/string/upperFirst.ts
@@ -0,0 +1,11 @@
+// 
+// Mettre le premier caractère en majuscule
+// 
+
+import upperFirst from "lodash/upperFirst"
+
+const prenom = 'olivier'
+
+const res = upperFirst(prenom)
+
+console.log(res)
